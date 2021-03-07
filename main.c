69bb35785af8134bb2e6f0f9d16bce05d5473a12
Nambinina13 <hery.andrianantenaina@ens.uvsq.fr>
2021-03-07 11:53:28 +0100

    Different file for projet top

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "capteur.h"
 
-int main(int argc, char **argv[])
+int main(int argc, char **argv)
 {
-  /* code */
+
+  int pid;
+  sscanf(argv[1], "%d", &pid);
+
+  printf("=============================================+==================\n");
+  printf("|   PID   |   Nom   |   S   |   Time   |   CPU   |   MEM   |     \n");
+  printf("================================================================\n");
+
+  process_value(pid);
   return 0;
 }
